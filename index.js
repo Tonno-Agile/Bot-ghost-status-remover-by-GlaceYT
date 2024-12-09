4eb765ff6aa5e61331b6f199c5e0b01a04a608df
Tonno-Agile <rikyspammer@gmail.com>
2024-12-09 18:48:23 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Sorvegliando tutto Mare Di Tonno 🐟", "Developing Mare Di Tonno 🍾", "Developed by Tonno 😀", "Guardando tutti gli Utenti" ];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
